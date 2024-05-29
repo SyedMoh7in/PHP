2e991104e3d6f68eb3054f7a0d0293627e072935
SyedMoh7in <149953029+SyedMoh7in@users.noreply.github.com>
2024-05-29 16:20:32 +0500

    Add files via upload
    This is the updated Practice for the Laracast series in which I have covered Section 1,2,3 and 4. The Notes Project CRUD functionalities have been covered until now .

---
+++ php-practice/Section1-Section 4/index.php
@@ -0,0 +1,13 @@
+<?php
+require "core/functions.php";
+// require "core/Database.php";
+// require "Response.php";
+require "../PHP-Practice-Level1/core/Router.php";
+
+$router= new \core\Router();
+
+$routes = require "routes.php";
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$method=$_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+$router->route($uri,'GET');
